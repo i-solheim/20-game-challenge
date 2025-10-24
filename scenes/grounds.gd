8dbe5aa1d70e5b2f39b17eb19a1e22c16598db75
i-solheim <lnhjpng@gmail.com>
2025-10-24 04:54:02 -0500

    create main, bird, and grounds scene

---
+++ scenes/grounds.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+class_name Ground
+
+@export var speed = -150
+
+@onready var ground1: Sprite2D = $Ground1/Sprite2D
+@onready var ground2: Sprite2D = $Ground2/Sprite2D
+
+
+func _ready():
+	ground2.global_position.x = ground1.global_position.x + ground1.texture.get_width()
+
+func _process(delta):
+	ground1.global_position.x += speed * delta
+	ground2.global_position.x += speed * delta
+	
+	if ground1.global_position.x < -ground1.texture.get_width():
+		ground1.global_position.x = ground2.global_position.x + ground2.texture.get_width()
+	if ground2.global_position.x < -ground2.texture.get_width():
+		ground2.global_position.x = ground1.global_position.x + ground1.texture.get_width()
